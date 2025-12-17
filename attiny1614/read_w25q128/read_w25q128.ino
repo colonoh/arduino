335edac19ad3b6503e30e30b79fe0bb13b8d78fa
Steve Warner <steve.j.warner@gmail.com>
2025-12-17 13:37:19 -0800

    Renaming variables for readability

--- attiny1614/read_w25q128/read_w25q128.ino
+++ attiny1614/read_w25q128/read_w25q128.ino
@@ -1,10 +1,10 @@
 #include <SPI.h>
 #include <avr/sleep.h>
 
-const int CS_PIN = 0;  // PA4
+const int MEM_CHIP_SELECT_PIN = 0;  // PA4
 const int LED_PIN = 5;
-const int SD_PIN = 7;  // PB0
-const int BTN_PIN = 4;  // PB3 Button to ground, uses internal pullup
+const int AMP_SHUTDOWN_PIN = 7;  // PB0
+const int BUTTON1_PIN = 4;  // PB3 Button to ground, uses internal pullup
 
 volatile uint32_t samplesRemaining = 0;
 volatile uint16_t avg = 0;
@@ -30,19 +30,19 @@ ISR(PORTB_PORT_vect) {
 
 void playAudio(uint32_t startAddr, uint32_t numSamples) {
   // Wake flash from deep power-down
-  digitalWrite(CS_PIN, LOW);
+  digitalWrite(MEM_CHIP_SELECT_PIN, LOW);
   SPI.transfer(0xAB);
-  digitalWrite(CS_PIN, HIGH);
+  digitalWrite(MEM_CHIP_SELECT_PIN, HIGH);
   delayMicroseconds(5);
 
   // Enable DAC and amp
   DAC0.CTRLA = DAC_ENABLE_bm | DAC_OUTEN_bm;
   DAC0.DATA = 128;
   delay(10);
-  digitalWrite(SD_PIN, HIGH);
+  digitalWrite(AMP_SHUTDOWN_PIN, HIGH);
 
   // Start flash read
-  digitalWrite(CS_PIN, LOW);
+  digitalWrite(MEM_CHIP_SELECT_PIN, LOW);
   SPI.transfer(0x03);
   SPI.transfer((startAddr >> 16) & 0xFF);
   SPI.transfer((startAddr >> 8) & 0xFF);
@@ -53,15 +53,15 @@ void playAudio(uint32_t startAddr, uint32_t numSamples) {
   while (samplesRemaining > 0);
   TCB0.CTRLA &= ~TCB_ENABLE_bm;
 
-  digitalWrite(CS_PIN, HIGH);
+  digitalWrite(MEM_CHIP_SELECT_PIN, HIGH);
   digitalWrite(LED_PIN, LOW);
   DAC0.DATA = 128;
 
   // Shutdown amp and flash
-  digitalWrite(SD_PIN, LOW);
-  digitalWrite(CS_PIN, LOW);
+  digitalWrite(AMP_SHUTDOWN_PIN, LOW);
+  digitalWrite(MEM_CHIP_SELECT_PIN, LOW);
   SPI.transfer(0xB9);
-  digitalWrite(CS_PIN, HIGH);
+  digitalWrite(MEM_CHIP_SELECT_PIN, HIGH);
   DAC0.CTRLA = 0;
 }
 
@@ -69,13 +69,13 @@ void goToSleep() {
   SPI.end();
 
   for (uint8_t i = 0; i < 11; i++) {
-    if (i == BTN_PIN) continue;  // Don't touch button pin
+    if (i == BUTTON1_PIN) continue;  // Don't touch button pin
     pinMode(i, OUTPUT);
     digitalWrite(i, LOW);
   }
-  digitalWrite(CS_PIN, HIGH);
+  digitalWrite(MEM_CHIP_SELECT_PIN, HIGH);
 
-  pinMode(BTN_PIN, INPUT_PULLUP);
+  pinMode(BUTTON1_PIN, INPUT_PULLUP);
   VPORTB.INTFLAGS = (1 << 3);
   PORTB.PIN3CTRL = PORT_PULLUPEN_bm | PORT_ISC_LEVEL_gc;
 
@@ -90,11 +90,11 @@ void goToSleep() {
 
 void setup() {
   pinMode(LED_PIN, OUTPUT);
-  pinMode(CS_PIN, OUTPUT);
-  pinMode(SD_PIN, OUTPUT);
-  pinMode(BTN_PIN, INPUT_PULLUP);
-  digitalWrite(CS_PIN, HIGH);
-  digitalWrite(SD_PIN, LOW);
+  pinMode(MEM_CHIP_SELECT_PIN, OUTPUT);
+  pinMode(AMP_SHUTDOWN_PIN, OUTPUT);
+  pinMode(BUTTON1_PIN, INPUT_PULLUP);
+  digitalWrite(MEM_CHIP_SELECT_PIN, HIGH);
+  digitalWrite(AMP_SHUTDOWN_PIN, LOW);
 
   SPI.begin();
 
